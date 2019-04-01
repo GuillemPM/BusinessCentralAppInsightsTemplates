29cb15789d69c45110ac5e65d387c55bbd4b737c
Christian Heide Damm <chrishd@microsoft.com>
2019-04-01 10:22:56 +0200

    x

---
+++ ReplaceFile.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string] $TemplateFile,
+
+    [Parameter(Mandatory=$true)]
+    [string] $OutputFile,
+
+    [Parameter(Mandatory=$true)]
+    [Guid] $SubscriptionId,
+
+    [Parameter(Mandatory=$true)]
+    [string] $ResourceGroupName,
+
+    [Parameter(Mandatory=$true)]
+    [string] $AppInsightsName
+)
+
+# Read input file
+$template = Get-Content -Path $TemplateFile -Raw
+
+# Replace
+$output = $template
+$output = $output -replace "<SUBSCRIPTIONID>",$SubscriptionId
+$output = $output -replace "<RESOURCEGROUPNAME>",$ResourceGroupName
+$output = $output -replace "<APPINSIGHTSNAME>",$AppInsightsName
+
+# Write output file
+$output | Out-File -FilePath $OutputFile -Encoding utf8
+Write-Host "Saved to: $OutputFile"
