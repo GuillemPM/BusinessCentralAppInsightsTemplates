b057ba6d0e2fe0c11b6b15c7f248cbde0ea38256
Christian Heide Damm <chrishd@microsoft.com>
2019-04-02 15:53:17 +0200

    x

---
+++ InternalScripts/PrepareAppInsightsTemplateForUpload.ps1
@@ -0,0 +1,6 @@
+.\ReplaceFile.ps1 `
+    -TemplateFile C:\repos\BusinessCentralAppInsightsTemplates\Dashboards\SimpleDashboard.json `
+    -OutputFile C:\repos\SimpleDashboard-ReadyToBeUploadedToAzurePortal.json `
+    -SubscriptionId b3a82977-e5a1-465a-92ca-fe89416c56cb `
+    -ResourceGroupName PartnerTelemetryResourceGroup `
+    -AppInsightsName partnertelemetry
