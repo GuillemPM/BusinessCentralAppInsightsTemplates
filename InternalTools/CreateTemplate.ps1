4eb597c25de5e75f2e2c2de7a5ffc7d4f1e55dec
Christian Heide Damm <chrishd@microsoft.com>
2019-04-03 16:45:40 +0200

    x

--- InternalTools/CreateTemplate.ps1
+++ InternalTools/CreateTemplate.ps1
@@ -8,4 +8,4 @@ $template = Get-Content -Path $DownloadedFile -Raw
 $template = $template -replace "b3a82977-e5a1-465a-92ca-fe89416c56cb","<SUBSCRIPTIONID>"
 $template = $template -replace "PartnerTelemetryResourceGroup","<RESOURCEGROUPNAME>"
 $template = $template -replace "partnertelemetry","<APPINSIGHTSNAME>"
-$template | Out-File -FilePath "C:\repos\BusinessCentralAppInsightsTemplates\Dashboards\SimpleDashboard.json" -Encoding utf8
+$template | Out-File -FilePath "C:\repos\BusinessCentralAppInsightsTemplates\DashboardsForVARs\BasicDashboard.json" -Encoding utf8
--- InternalTools/PrepareAppInsightsTemplateForUpload.ps1
+++ InternalTools/PrepareAppInsightsTemplateForUpload.ps1
@@ -1,5 +1,5 @@
-.\ReplaceFile.ps1 `
-    -TemplateFile C:\repos\BusinessCentralAppInsightsTemplates\Dashboards\SimpleDashboard.json `
+c:\repos\BusinessCentralAppInsightsTemplates\PrepareDashboard.ps1 `
+    -TemplateFile C:\repos\BusinessCentralAppInsightsTemplates\DashboardsForVARs\BasicDashboard.json `
     -OutputFile C:\repos\SimpleDashboard-ReadyToBeUploadedToAzurePortal.json `
     -SubscriptionId b3a82977-e5a1-465a-92ca-fe89416c56cb `
     -ResourceGroupName PartnerTelemetryResourceGroup `
